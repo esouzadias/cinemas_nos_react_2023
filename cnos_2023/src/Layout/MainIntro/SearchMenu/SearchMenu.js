972f826314976b9bc417f5a418ac94bd6d42283b
Eric Dias <esouzadias96@gmail.com>
2023-09-26 09:23:52 +0100

    Header

---
+++ cnos_2023/src/Layout/MainIntro/SearchMenu/SearchMenu.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./searchMenu.scss"
+
+function searchMenu() {
+  return (
+    <div className="menu-bar">
+      <ul>
+        <li> Filmes </li>
+        <li> Cinemas </li>
+        <li> Bar </li>
+        <li> Outros Produtos </li>
+      </ul>
+    </div>
+  )
+}
+
+export default searchMenu
