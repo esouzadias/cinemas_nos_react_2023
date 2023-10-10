ffd8107cd052540143efddf42053056c8825e059
Eric Dias <esouzadias96@gmail.com>
2023-10-10 17:17:25 +0100

    Update MainIntro

--- cnos_2023/src/Layout/MainIntro/MainIntro.js
+++ cnos_2023/src/Layout/MainIntro/MainIntro.js
@@ -1,3 +1,6 @@
+//* SUMARY: ESTE COMPONENTE RECOLHE OS FILES E A SUA INFORMAÇÃO COM BASE NA API E APRESENTAR EM FORMATO DE SLIDER TODOS OS FILMES QUE ESTÃO NESTE MOMENTO NOS CINEMAS.
+//* ALÉM DISSO, PERMITE SELECIONAR O FILME E VER O TRAILER DISPONÍVEL E COMPRAR BILHETE A PARTIR DA MESMA JANELA. =================================================== 
+
 import React, { useState, useEffect } from 'react'
 import './MainIntro.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -5,12 +8,14 @@ import { faPlay } from '@fortawesome/free-solid-svg-icons'
 import { faClose } from '@fortawesome/free-solid-svg-icons'
 import { fetchMovieTrailersData } from '../../Services/Utils/ApiRequests.js';
 
-function MainIntro({ mainMovie, currentMovieIndex, currentMovieGenre, movieTime }) {
-  const [backgroundImage, setBackgroundImage] = useState(mainMovie && (`url(https://image.tmdb.org/t/p/w500${mainMovie[currentMovieIndex].poster_path})`));
+function MainIntro({ mainMovie, currentMovieIndex, currentMovieGenre, movieTime, isAdult }) {
+  const [backgroundImage, setBackgroundImage] = useState(mainMovie && (`url(https://image.tmdb.org/t/p/w500${mainMovie[currentMovieIndex]?.backdrop_path})`));
   const [trailerPlaying, setTrailerPlaying] = useState(false);
-  const [currentMovie, setCurrentMovie] = useState(mainMovie[0]);
+  const [currentMovie, setCurrentMovie] = useState(mainMovie ? mainMovie[0] : null);
   const [movieTrailersData, setMovieTrailersData] = useState(null);
 
+
+  // Muda o backdrop para um video player e corre o trailer do filme selecionado
   const handleTrailerClick = (e) => {
     e.currentTarget.style.display = 'none';
     e.currentTarget.parentElement.style.transition = "width 0.5s ease-in-out";
@@ -25,11 +30,13 @@ function MainIntro({ mainMovie, currentMovieIndex, currentMovieGenre, movieTime 
   }, 500);
   }
 
+  // fecha o video player e volta ao estado default do slider
   const handleCloseClick = (e) => {
     setTrailerPlaying(false);
     e.currentTarget.parentElement.style.paddingTop = "50px";
   }
 
+  // recolhe os trailes de todos os filmes
   const fetchTrailers = async () => {
     try {
       const movieTrailersResponse = await fetchMovieTrailersData(currentMovie.id);
@@ -39,23 +46,23 @@ function MainIntro({ mainMovie, currentMovieIndex, currentMovieGenre, movieTime 
     }
   }
 
+  // Configura um temporizador para alterar o plano de fundo a cada x segundos
   useEffect(() => {
     if (!trailerPlaying) {
-      mainMovie && (setBackgroundImage(`url(https://image.tmdb.org/t/p/w500${mainMovie[currentMovieIndex].poster_path})`));
+      mainMovie && (setBackgroundImage(`url(https://image.tmdb.org/t/p/w500${mainMovie[currentMovieIndex].backdrop_path})`));
       setCurrentMovie(mainMovie[currentMovieIndex]);
 
-      // Configurar um temporizador para alterar o plano de fundo a cada x segundos
       const timerId = setTimeout(() => {
-        const nextIndex = (currentMovieIndex + 1) % 6; // Alternar entre os 6 filmes
+        const nextIndex = (currentMovieIndex + 1) % 6; // Alterna entre cada 6 filmes
         setBackgroundImage(
           mainMovie && mainMovie[nextIndex]
-            ? `url(https://image.tmdb.org/t/p/w500${mainMovie[nextIndex].poster_path})`
+            ? `url(https://image.tmdb.org/t/p/w500${mainMovie[nextIndex].backdrop_path})`
             : ''
         );
-      }, 10000); // Alterar a cada 5 segundos (ou o que desejar)
+      }, 10000); // Altera a cada 5 segundos
 
       return () => {
-        // Limpar o temporizador quando o componente é desmontado ou quando o índice muda
+        // Limpa o temporizador quando o componente é desmontado ou quando o index muda
         clearTimeout(timerId);
       };
     }
@@ -66,22 +73,22 @@ function MainIntro({ mainMovie, currentMovieIndex, currentMovieGenre, movieTime 
       {trailerPlaying && (<FontAwesomeIcon id='closeBtn' onClick={handleCloseClick.bind(this)} icon={faClose} />)}
       <div id='main-intro-content'>
         {!trailerPlaying ? (
-          <div onClick={handleTrailerClick} id={'video-player'} style={{ backgroundImage: backgroundImage, backgroundSize: 'cover' }}>
+          <div onClick={handleTrailerClick} id={'video-player'} style={{ backgroundImage: backgroundImage, /* backgroundSize: 'cover' */ }}>
             <FontAwesomeIcon className='icon' icon={faPlay} />
           </div>
         ) : (
         <>
-          <iframe width="100%" height="80%" src={movieTrailersData && `https://www.youtube.com/embed/${movieTrailersData[4].key}?autoplay=1`} />
+          <iframe width="100%" height="80%" src={movieTrailersData && `https://www.youtube.com/embed/${movieTrailersData[4]?.key}?autoplay=1`} />
         </>
         )}
         {!trailerPlaying && (
           <div className='movie-info'>
             <div className='movie-info-container'>
-              <span>M16 • {currentMovieGenre ? currentMovieGenre : null} | {movieTime ? movieTime : null}</span>
+              <span>{isAdult ? 'M18' : 'M16'} • {currentMovieGenre ? currentMovieGenre : null} | {mainMovie[currentMovieIndex]?.duration} Min</span>
               <h2>{mainMovie[currentMovieIndex]?.original_title}</h2>
               <span>{mainMovie[currentMovieIndex]?.overview}</span>
               <div>
-                Língua: {mainMovie[currentMovieIndex].original_language}
+                Língua: {mainMovie[currentMovieIndex]?.original_language}
                 <button>Comprar Bilhete</button>
               </div>
             </div>
